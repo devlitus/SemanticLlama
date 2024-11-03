c38a896541d2afabc339e72b05b9a711ca218a0c
devlitus <developercarles@gmail.com>
2024-11-03 08:06:58 +0100

    añadir configuración vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
+    setupFiles: ["./vitest.setup.ts"],
+    deps: {
+      inline: [/pdf\.js/],
     },
     alias: {
       "@/": "/src/",
